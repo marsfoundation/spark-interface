b05a44ecff50b42e5cf2ee3997455658d7acde0e
Sam MacPherson <xylom44@gmail.com>
2023-03-07 06:12:45 -0700

    fix faq link

--- src/layouts/AppHeader.tsx
+++ src/layouts/AppHeader.tsx
@@ -70,7 +70,7 @@ export function AppHeader() {
       <Typography variant="description">
         <Trans>The app is running in testnet mode. Learn how it works in</Trans>{' '}
         <Link
-          href="https://docs.sparkprotocol.io/faq/testing-aave"
+          href="https://docs.sparkprotocol.io/faq/testing-spark-lend"
           style={{ fontSize: '14px', fontWeight: 400, textDecoration: 'underline' }}
         >
           FAQ.
