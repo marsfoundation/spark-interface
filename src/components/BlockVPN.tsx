099a7ac57e2b9d5c4a646fb6786c8cf8707ce3dd
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-07-26 12:54:09 +0200

    Ensure geoip script is loaded

--- src/components/BlockVPN.tsx
+++ src/components/BlockVPN.tsx
@@ -9,19 +9,24 @@ export function BlockVPN({ children }: { children: React.ReactNode }): React.Rea
   useEffect(() => {
     // executes only on client
     if (typeof window !== 'undefined') {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const geoip2 = (window as any).geoip2;
+      const geoipScript = document.querySelector('#geoip-script');
+      geoipScript?.addEventListener('load', function () {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const geoip2 = (window as any).geoip2;
 
-      geoip2.insights(
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (insights: any) => {
-          setIsVpn(insights.traits.user_type.toLowerCase() === 'hosting');
-        },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (error: any) => {
-          console.error('Error while detecting VPN', error);
-        }
-      );
+        geoip2.insights(
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (insights: any) => {
+            const isVpn = insights.traits.user_type.toLowerCase() === 'hosting';
+            console.log('Vpn detected: ', isVpn);
+            setIsVpn(isVpn);
+          },
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (error: any) => {
+            console.error('Error while detecting VPN', error);
+          }
+        );
+      });
     }
   }, []);
 
